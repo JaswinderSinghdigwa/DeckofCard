6129033f44112dcdc2a20d873b16dc34f44e73c9
Aman <you@example.com>
2021-10-03 14:55:01 +0530

    Intializing the size of Deck of card

--- src/com/bl/deckofcard/DeckOfCard.java
+++ src/com/bl/deckofcard/DeckOfCard.java
@@ -1,36 +1,41 @@
-/******************************************************************************
- *  Purpose: Program is written for shuffle and distribute card of deck
- *
- *  @author  Jaswinder Singh
- *  @version 1.0
- *  @since   01-10-2020
- *
- ******************************************************************************/
-package com.bl.deckofcard;
-
-public class DeckOfCard {
-	 String[] cardType = { "Clubs" , "Diamonds" , "Heart" ,"Spade"};
-	 String[] cardNumber =  { "2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
-	 String[] cardArray = new String[52];
-	 int start= 0,end =12 ,j1 =0;
-	 
-	 public void rank() {
-	 for (int i = 0;i < 4; i++) {
-		 for (int j = start; j <= end;j++) {
-			cardArray[j]= cardType[i] + " " + cardNumber[j1]; 
-			j1++;
+	/******************************************************************************
+	 *  Purpose: Program is written for shuffle and distribute card of deck
+	 *
+	 *  @author  Jaswinder Singh
+	 *  @version 1.0
+	 *  @since   01-10-2020
+	 *
+	 ******************************************************************************/
+	package com.bl.deckofcard;
+	
+	public class DeckOfCard {
+		 String[] suit = { "Clubs" , "Diamonds" , "Heart" ,"Spade"};
+		 String[] rank =  { "2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+		 
+		 //initialized the size of cards
+		 String[] cards = new String[52];
+		 private int start= 0,end =12 ,count =0;
+		 
+		 // creating the quiuness of cards
+		 public void DeckofcardGame() {
+		 for (int i = 0;i < 4; i++) {
+			 for (int j = start; j <= end;j++) {
+				cards[j]= suit[i] + " " + rank[count]; 
+				count++;
+			 }
+			 count = 0;
+			 start = end+1;
+			 end +=13;
+		 	}
+		}
+		 
+		 //Displaying cards and rank ,suit 
+		  public void printCard() {
+		
+			  for (int i =0;i<cards.length;i++) //loop to print all the element of ArrayList
+			  {
+				  System.out.println(cards[i]+"");
+			  }
+		
 		 }
-		 j1 = 0;
-		 start = end+1;
-		 end +=13;
-	 	}
-	 }
-	 
-	  public void print() {
-	
-		  for (int i =0;i<cardArray.length;i++) {
-			  System.out.println(cardArray[i]+"");
-		  }
-	
-	  }
 }
---
+++ src/com/bl/deckofcard/DeckofCard.java
@@ -0,0 +1,10 @@
+package com.bl.deckofcard;
+
+public class DeckofCard {
+	public static void main(String[] args) {
+		DeckOfCard deckofcard = new DeckOfCard();
+		System.out.print("Welcom to Deck Of Crad");
+		deckofcard.DeckofcardGame();
+		deckofcard.printCard();
+	}
+}
