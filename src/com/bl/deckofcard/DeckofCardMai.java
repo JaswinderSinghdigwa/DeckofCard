3cf4429f1959b325c999c143eb5f530d2215756e
Aman <you@example.com>
2021-10-03 14:56:38 +0530

    Intializing the size of Deck of card

--- src/com/bl/deckofcard/DeckofCardMai.java
+++ src/com/bl/deckofcard/DeckofCardMai.java
@@ -1,6 +1,6 @@
 package com.bl.deckofcard;
 
-public class DeckofCard {
+public class DeckofCardMain {
 	public static void main(String[] args) {
 		DeckOfCard deckofcard = new DeckOfCard();
 		System.out.print("Welcom to Deck Of Crad");
